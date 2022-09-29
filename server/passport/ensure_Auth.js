336dfbaf45048e694ea0026f5e434a4fca838b53
mumu <mumuwolgan@gmail.com>
2022-09-29 20:41:59 +0900

    function ensure_login

---
+++ server/passport/ensure_Auth.js
@@ -0,0 +1,10 @@
+let ensureAuthenticated = (req, res, next) => {
+    if (req.isAuthenticated()){
+        next()
+    }
+    else{
+        res.redirect("/silpae")
+    }
+}
+
+module.exports = ensureAuthenticated
--- server/router/main.js
+++ server/router/main.js
@@ -1,9 +1,13 @@
 const router = require("express").Router()
+const ensure_auth = require("../passport/ensure_Auth")
 
 router.get('/', function (req, res){
     res.send("로그인됐나?")
 })
 router.get('/silpae', function (req, res){
     res.send("로그인안됐나?")
+})
+router.get('/loginpilsoo', ensure_auth, function(req,res){
+    res.send("용케 들어왔네요")
 })
 module.exports = router
