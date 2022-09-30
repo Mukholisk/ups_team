57bc04287bed4114623d5118b4bf56337f4fa50d
mumu <bshookhu@gmail.com>
2022-09-30 20:05:07 +0900

    fix err

--- server/server.js
+++ server/server.js
@@ -1,43 +1,25 @@
 const express = require("express"); // express 모듈
 const session = require("express-session");
-<<<<<<< HEAD
-const passport = require('passport');
-const db = require('./conn/conn_mongo');
-=======
 const passport = require("passport");
 const db = require("./conn/conn_mongo");
->>>>>>> 4263544 (merge)
 const bodyParser = require("body-parser");
 
 const setUpPassport = require("./passport/config_passport");
 
-<<<<<<< HEAD
-const main = require('./router/main');
-const naver = require('./router/auth/naver');
-const login = require('./router/auth/login');
-const register = require('./router/register/register');
-=======
 const main = require("./router/main");
 const naver = require("./router/api/auth/naver");
-const ups = require("./router/api/auth/login");
+const ups = require("./router/api/auth/ups");
 const register = require("./router/api/register/register");
->>>>>>> 4263544 (merge)
 const app = express();
 const PORT = 1128;
 
 setUpPassport();
 
-<<<<<<< HEAD
-app.use(express.urlencoded({
-	extended: true
-}));
-=======
 app.use(
   express.urlencoded({
     extended: true,
   })
 );
->>>>>>> 4263544 (merge)
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -54,15 +36,6 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-<<<<<<< HEAD
-app.use('/auth/naver', naver);
-app.use('/auth/login', login);
-app.use('/', main);
-app.use('/register', register);
-
-app.listen(PORT, () => {
-    console.log(`Server is on PORT Number ${PORT}`);
-=======
 app.use("/api/auth/naver", naver);
 app.use("/api/auth/ups", ups);
 app.use("/", main);
@@ -70,5 +43,4 @@ app.use("/api/register", register);
 
 app.listen(PORT, () => {
   console.log(`Server is on PORT Number ${PORT}`);
->>>>>>> 4263544 (merge)
 });
