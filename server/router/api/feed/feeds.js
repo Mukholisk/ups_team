5a22a9a89f897a97c8a7c7e61096395a5813b788
mumu <bshookhu@gmail.com>
2022-10-01 00:18:06 +0900

    feed get

--- server/router/api/feed/feeds.js
+++ server/router/api/feed/feeds.js
@@ -1,36 +1,50 @@
 const router = require("express").Router();
-const passport = require("passport");
 const Feeds = require("../../../Models/Feeds");
-const LocalStrategy = require("passport-local").Strategy;
 
-router.get("/", (req, res)=>{
-    const x = req.param('x');
-    const y = req.param('y');
-    res.send('지도가 떠야하는 곳<br>' + 'x: ' + x + ' y: ' + y);
+router.get("/", (req, res) => {
+  let min_x = parseFloat(req.params["x"]) - 0.002;
+  let max_x = parseFloat(req.params["x"]) + 0.002;
+  let min_y = parseFloat(req.params["y"]) - 0.0025;
+  let max_y = parseFloat(req.params["y"]) + 0.0025;
+
+  Feeds.find(
+    {
+      location: {
+        x: { $gte: min_x, $lte: max_x },
+        y: { $gte: min_y, $lte: max_y },
+      },
+    },
+    (err, feeds_loc) => {
+      if (err)
+        return res.status(404).json({
+          success: false,
+          err,
+        });
+      return res.status(200).send(feeds_loc);
+    }
+  );
 });
 
-router.get('/:feed_id', (req, res)=>{
-    const info = req.params.feed_id;
-    // DB에서 정보 불러오기?
-    res.send(info + '번 마커찔렀을때');
+router.get("/:feed_id", (req, res) => {
+  const info = req.params.feed_id;
+  // DB에서 정보 불러오기?
+  res.send(info + "번 마커찔렀을때");
 });
 
-router.post('/', (req, res)=>{
+router.post("/", (req, res) => {
   const ID = req.session.passport.user.account_id;
-  const x = req.param('x');
-  const y = req.param('y');
-  let query = Feeds.visits.update({user_id: ID}, {$set: {
-    location: {x: x, y: y}
-  }});
+  const x = req.param("x");
+  const y = req.param("y");
+  let query = Feeds.visits.update(
+    { user_id: ID },
+    {
+      $set: {
+        location: { x: x, y: y },
+      },
+    }
+  );
 });
 
-router.post('/:feed_id/emotions', (req, res)=>{
-
-});
-
-passport.authenticate("local-login", {
-    successRedirect: "/",
-    failureRedirect: "/silpae",
-});
+router.post("/:feed_id/emotions", (req, res) => {});
 
 module.exports = router;
